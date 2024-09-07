65e5f0bdd9286960930aad4011c9eb6c17dac6a2
peaceboy9914 <142726336+peaceboy9914@users.noreply.github.com>
2024-09-07 22:07:43 +0300

    added image upload api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GjobModule } from './gjob/gjob.module';
 import { ConfigModule } from '@nestjs/config';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), GjobModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), GjobModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
