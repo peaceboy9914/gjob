a04c68dcea943574941640b5492da95c297f570a
peaceboy9914 <142726336+peaceboy9914@users.noreply.github.com>
2024-09-09 23:29:15 +0300

    user crud finalized

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GjobModule } from './gjob/gjob.module';
 import { ConfigModule } from '@nestjs/config';
 import { ItemsModule } from './items/items.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), GjobModule, ItemsModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
