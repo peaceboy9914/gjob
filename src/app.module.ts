a3665f9da73584853b98a9e121a06b612377a2a7
peaceboy9914 <142726336+peaceboy9914@users.noreply.github.com>
2024-09-09 19:25:05 +0300

    item schema merged to gjob schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GjobModule } from './gjob/gjob.module';
 import { ConfigModule } from '@nestjs/config';
-import { ImagesModule } from './images/images.module';
 import { ItemsModule } from './items/items.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), GjobModule, ImagesModule, ItemsModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_URL), GjobModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
