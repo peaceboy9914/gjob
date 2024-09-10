bd3f973541bc3c54317c9d29ea48dad8279151c4
peaceboy9914 <142726336+peaceboy9914@users.noreply.github.com>
2024-09-11 00:02:08 +0300

    Working on Authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/user/Schema/user.schema';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{name: User.name, schema: UserSchema}])
+        MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+        JwtModule.register({
+            secret: process.env.JJWT_SECRET_KEY,
+            signOptions: { expiresIn: '1h' },
+        })
     ],
     controllers: [AuthController],
-    providers: [UserService]
+    providers: [UserService, AuthService],
+    exports: [AuthService],
 })
 export class AuthModule {}
